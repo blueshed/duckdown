3184d2219c490d07647779edba0da815898ddaed
Peter Bunyan <pete@blueshed.co.uk>
2020-11-27 17:49:45 +0000

    Bump version: 0.0.39 → 0.0.40

--- duckdown/__init__.py
+++ duckdown/__init__.py
@@ -1,3 +1,3 @@
 """ duckdown app """
 
-VERSION = "0.0.39"
+VERSION = "0.0.40"
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="duckdown",
-    version="0.0.39",
+    version="0.0.40",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/blueshed/duckdown/",
